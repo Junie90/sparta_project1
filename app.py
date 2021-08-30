3ac918195344b95b10f18d8414eb3b5fe91c2633
Junie90 <53658055+Junie90@users.noreply.github.com>
2021-08-30 23:52:30 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+import requests
+
+from bs4 import BeautifulSoup
+
+app: Flask = Flask(__name__)
+client = MongoClient('mongodb://test:test@3.38.93.118', 27017)
+db = client.dbsparta
+
+
+# HTML 을 주는 부분
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/api/cafes', methods=['GET'])
+def listing():
+    location = request.args['location']
+    query = {
+        "address": {
+            "$regex": f"{ location }"
+        }
+    }
+    cafes = list(db.cafes.find(query, {'_id': False}))
+    return jsonify({"message": "좋았어 성공이야", "cafes": cafes})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
